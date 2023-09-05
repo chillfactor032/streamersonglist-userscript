54b7bb9995c9d8cb39f990fc8bbe5dc2f73057de
chillfactor032 <68230787+chillfactor032@users.noreply.github.com>
2023-09-05 13:16:37 -0500

    Update ssl_userscript.js

--- ssl_userscript.js
+++ ssl_userscript.js
@@ -7,7 +7,6 @@
 // @homepage    https://github.com/chillfactor032/streamersonglist-userscript
 // @match       https://www.streamersonglist.com/*
 // @icon        https://www.streamersonglist.com/assets/icon/favicon-96x96.png
-// @updateURL   https://raw.githubusercontent.com/chillfactor032/streamersonglist-userscript/main/version.js
 // @downloadURL https://raw.githubusercontent.com/chillfactor032/streamersonglist-userscript/main/ssl_userscript.js
 // @supportURL  https://github.com/chillfactor032/streamersonglist-userscript/issues
 // @run-at      document-idle
